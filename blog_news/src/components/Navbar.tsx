4713e5aa844e148cf018be70e023fe0a53b71d85
agggutie <aggutieDev@gmail.com>
2024-06-26 14:57:11 -0400

    aws config

--- blog_news/src/components/Navbar.tsx
+++ blog_news/src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import Link from 'next/link';
-import { useAuth } from '@/utils/authContext';
+import { useAuth } from '@/utils/AuthContext';
 
 const Navbar = () => {
   const { user, handleLogout } = useAuth();
