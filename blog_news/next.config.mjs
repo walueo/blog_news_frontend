68484b40dab8ea700b18a7004c832702e6839185
agggutie <aggutieDev@gmail.com>
2024-07-09 11:55:15 -0400

    serverless

--- blog_news/next.config.mjs
+++
@@ -1,7 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-export default {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone', // Updated as per the new configuration
-};
